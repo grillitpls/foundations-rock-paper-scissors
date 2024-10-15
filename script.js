0c1ff4a7178ff4e554515b859074ca5065b84810
grillitpls <102038067+grillitpls@users.noreply.github.com>
2024-10-15 15:36:17 +0700

    fixing getHumanChoice

--- script.js
+++ script.js
@@ -16,9 +16,7 @@ function getComputerChoice() {
 
 // get human choice
 function getHumanChoice() {
-  humanChoice = prompt("Enter one of these to pick: Rock, Paper, Scissors")
-  
-  humanChoice.toUpperCase()
+  humanChoice = prompt("Enter one of these to pick: Rock, Paper, Scissors").toUpperCase()
 
   if (humanChoice !== "ROCK" && humanChoice !== "PAPER" && humanChoice !== "SCISSORS") {
     humanChoice = prompt("Have to be one of these: Rock, Paper, Scissors").toUpperCase()
